80f38d88bca55c7923ba397afc73a85c5a1580f7
ungnhan14031999 <ungnhan14031999@gmail.com>
2024-02-17 22:06:24 +0700

    fix build frontend source

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-// import theme from "tailwind-theme";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles/styles.scss';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
-);
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+import IntlProviderWrapper from "./hoc/IntlProviderWrapper";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+import { Provider } from 'react-redux';
+import reduxStore, { persistor } from './redux';
+
+const renderApp = () => {
+    ReactDOM.render(
+        <Provider store={reduxStore}>
+            <IntlProviderWrapper>
+                <App persistor={persistor}/>
+            </IntlProviderWrapper>
+        </Provider>,
+        document.getElementById('root')
+    );
+};
+
+renderApp();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
